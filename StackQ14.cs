f642194e1f40f9af535da49053a159a0a1b94b23
mariam mahboub <110869557+mariammahboub@users.noreply.github.com>
2023-06-27 20:57:51 -0500

    StackQ14.cs

---
+++ StackQ14.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        Console.WriteLine("Stack Before left rotation:");
+        PrintStack(stack);
+        RotateLeft(stack);
+        Console.WriteLine("Stack after left rotation first three elments in stack :");
+        PrintStack(stack);
+    }
+    static void RotateLeft(Stack<int> stack)
+    {
+        if (stack.Count <= 1)
+            return;
+        LinkedList<int> linkedList = new LinkedList<int>(stack);
+        LinkedListNode<int> firstNode = linkedList.First;
+        linkedList.RemoveFirst();
+        linkedList.AddLast(firstNode);
+        stack.Clear();
+        foreach (int element in linkedList)
+        {
+            stack.Push(element);
+        }
+    }
+    static void PrintStack(Stack<int> stack)
+    {
+        foreach (int element in stack)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
