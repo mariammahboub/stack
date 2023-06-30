c32e557f1f0db7a06664375acea0d45afd7de3a5
mariam mahboub <110869557+mariammahboub@users.noreply.github.com>
2023-06-30 16:01:26 -0500

    StackQ13.cs

--- StackQ13.cs
+++ StackQ13.cs
@@ -1,47 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 namespace ConsoleApp1
 {
-    internal class StackQ2{
-    class Program
+    internal class StackQ13
     {
-        static void Main(string[] args)
+        class Program
         {
-            Stack<int> stack = new Stack<int>();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-            stack.Push(4);
-            stack.Push(5);
-            int topElement = stack.Peek();
-            int bottomElement = FindBottomElement(stack);
-            Console.WriteLine("Top Element: " + topElement);
-            Console.WriteLine("Bottom Element: " + bottomElement);
-        }
-        static int FindBottomElement(Stack<int> stack)
-        {
-            if (stack.Count == 0)
+            static void Main(string[] args)
             {
-                Console.WriteLine("Stack is empty");
-                return -1; 
+                Stack<int> stack = new Stack<int>();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+                stack.Push(4);
+                stack.Push(8);
+                int topElement = stack.Peek();
+                int bottomElement = FindTopElement(stack);
+                Console.WriteLine("Top Element: " + bottomElement);
+                Console.WriteLine("Bottom Element: " + topElement);
             }
-            int bottomElement = stack.Peek();
-            Stack<int> tempStack = new Stack<int>();
-            while (stack.Count > 0)
+            static int FindTopElement(Stack<int> stack)
             {
-                int element = stack.Pop();
-
                 if (stack.Count == 0)
-                    bottomElement = element;
+                {
+                    Console.WriteLine("Stack is empty");
+                    return -1;
+                }
+                int bottomElement = stack.Peek();
+                Stack<int> tempStack = new Stack<int>();
+                while (stack.Count > 0)
+                {
+                    int element = stack.Pop();
 
-                tempStack.Push(element);
+                    if (stack.Count == 0)
+                        bottomElement = element;
+
+                    tempStack.Push(element);
+                }
+                while (tempStack.Count > 0)
+                {
+                    stack.Push(tempStack.Pop());
+                }
+                return bottomElement;
             }
-            while (tempStack.Count > 0)
-            {
-                stack.Push(tempStack.Pop());
-            }
-            return bottomElement;
         }
     }
-}
 }
