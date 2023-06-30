c6c68b1ca367e2721fc6298f007cc5fbbacafcf4
mariam mahboub <110869557+mariammahboub@users.noreply.github.com>
2023-06-30 15:54:27 -0500

    StackQ12.cs

--- StackQ12.cs
+++ StackQ12.cs
@@ -6,50 +6,42 @@ namespace ConsoleApp1
 {
     internal class StackQ9
     {
-    public class StackDemo
-    {
-        private List<int> data;
-        private int top;
-        private int capacity;
-
-        public StackDemo()
+        public class StackDemo
         {
-            data = new List<int>();
-            top = -1;
-            capacity = 0;
-        }
-
-        public void Push(int item)
-        {
-            capacity++;
-            top++;
-            data.Add(item);
-        }
-
-        public void RemoveDuplicite()
-        {
-
-            int[] temp = new int[] { };
-            Stack theTemp = new Stack();
-            for (int i = 0; i < top; i++)
+            private List<int> data;
+            private int top;
+            public StackDemo()
             {
-                if (!temp.Contains(data[i]))
-                { 
+                data = new List<int>();
+                top = -1;
+            }
+            public void Push(int item)
+            {
+                top++;
+                data.Add(item);
+            }
+            public void RemoveDuplicite()
+            {
 
+                int[] temp = new int[] { };
+                Stack theTemp = new Stack();
+                for (int i = 0; i < top; i++)
+                {
+                    if (!temp.Contains(data[i]))
+                    {
+
+                    }
                 }
             }
-        }
-
             public static void Main(string[] args)
-        {
-            StackDemo stack = new StackDemo();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-            stack.RemoveDuplicite();
-            Console.WriteLine("Duplicates removed.");
+            {
+                StackDemo stack = new StackDemo();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+                stack.RemoveDuplicite();
+                Console.WriteLine("Duplicates removed.");
+            }
         }
     }
 }
-}
-
